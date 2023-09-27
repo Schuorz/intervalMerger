8bf4f7e68a55cb50d43b4bf093312c77a7256599
Max Schwarz <xd3max@gmail.com>
2023-09-27 18:07:18 +0200

    fixed bug

--- main_test.go
+++ main_test.go
@@ -108,6 +108,18 @@ func TestMERGE(t *testing.T) {
 			want:    [][]int{{1, 5}},
 			wantErr: false,
 		},
+		{
+			name:    "Successive merge",
+			args:    [][]int{{1, 4}, {6, 8}, {3, 7}},
+			want:    [][]int{{1, 8}},
+			wantErr: false,
+		},
+		{
+			name:    "Everything together",
+			args:    [][]int{{1, 4}, {1, 4}, {6, 8}, {7, 10}, {12, 14}, {15, 18}, {19, 22}, {23, 25}, {3, 7}, {11, 26}, {1, 3}, {3, 4}},
+			want:    [][]int{{1, 10}, {11, 26}},
+			wantErr: false,
+		},
 	}
 
 	for _, tt := range tests {
